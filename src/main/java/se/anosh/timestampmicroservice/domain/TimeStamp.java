003e942736e168fb7329204f91ee97f7e2c0ad63
Anosh D. Ullenius <anosh@anosh.se>
2019-02-19 17:16:30 +0100

    Implementing Comparable

--- src/main/java/se/anosh/timestampmicroservice/domain/TimeStamp.java
+++ src/main/java/se/anosh/timestampmicroservice/domain/TimeStamp.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author Anosh D. Ullenius <anosh@anosh.se>
  */
 @XmlRootElement
-public class TimeStamp {
+public class TimeStamp implements Comparable<TimeStamp> {
     
     private long unix;
     private String utc;
@@ -33,6 +33,11 @@ public class TimeStamp {
 	public String toString() {
 		return "TimeStamp [unix=" + unix + ", utc=" + utc + "]";
 	}
+
+    @Override
+    public int compareTo(TimeStamp t) {
+        return Long.compare(unix, t.getUnix());
+    }
     
     
 }
